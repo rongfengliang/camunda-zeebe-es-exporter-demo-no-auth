d999408b2c3eed1539165ab542cbcf82b9c71b6d
rongfengliang <1141591465@qq.com>
2019-04-26 21:41:56 +0800

    add demo

---
+++ exporter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/zeebe-io/zeebe-simple-monitor/releases/download/0.13.0/zeebe-simple-monitor-exporter-0.13.0.jar
+wget https://github.com/zeebe-io/zeebe-hazelcast-exporter/releases/download/0.2.0/zeebe-hazelcast-exporter-0.2.0.jar
